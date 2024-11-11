3670221c53b3400630764c2cdfcbff21e09082c6
unknown <heric.moura@unifesp.br>
2024-11-11 20:30:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -22,60 +22,71 @@ const cameras = {
   "19": {"lugar": "Câmera 0436", "url": "https://egov.santos.sp.gov.br/santosmapeada/css/img/cameras/cam0436/snap_c1.jpg?1731351036758"},
   "20": {"lugar": "Acesso a Praia Canal 1", "url": "https://egov.santos.sp.gov.br/santosmapeada/css/img/cameras/cam0416/snap_c1.jpg?1731353613270"},
   "21": {"lugar": "Praça das Bandeiras", "url": "https://egov.santos.sp.gov.br/santosmapeada/css/img/cameras/cam0443/snap_c1.jpg?1731353706967"}
+  "22": {"lugar": "Canal 4", "url": "https://egov.santos.sp.gov.br/santosmapeada/css/img/cameras/cam1529/snap_c1.jpg?1731367713141"},
 };
 
 function App() {
-    const [cameraUrls, setCameraUrls] = useState(Object.values(cameras));
-    const [isFullScreen, setIsFullScreen] = useState(false);
-    const [currentImage, setCurrentImage] = useState(null);
+  const [cameraUrls, setCameraUrls] = useState(Object.values(cameras));
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
 
-    useEffect(() => {
-        const updateImages = () => {
-            const updatedCameras = Object.keys(cameras).map(key => ({
-                ...cameras[key],
-                url: `${cameras[key].url}&t=${new Date().getTime()}` // Atualiza a URL com timestamp para evitar cache
-            }));
-            setCameraUrls(updatedCameras);
-        };
-
-        const interval = setInterval(updateImages, 3000); // Atualiza a cada 3 segundos
-        updateImages(); // Atualiza uma vez antes de iniciar o intervalo
-
-        return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
-    }, []);
-
-    const handleImageClick = (url) => {
-        setCurrentImage(url);
-        setIsFullScreen(true);
+  useEffect(() => {
+    const updateImages = () => {
+      const updatedCameras = Object.keys(cameras).map(key => ({
+        ...cameras[key],
+        url: `${cameras[key].url}&t=${new Date().getTime()}` // Atualiza a URL com timestamp para evitar cache
+      }));
+      setCameraUrls(updatedCameras);
     };
 
-    const closeFullScreen = () => {
-        setIsFullScreen(false);
-        setCurrentImage(null);
-    };
+    const interval = setInterval(updateImages, 3000); // Atualiza a cada 3 segundos
+    updateImages(); // Atualiza uma vez antes de iniciar o intervalo
 
-    return (
-      <div className="min-h-screen bg-gray-200 p-0 flex flex-col items-center justify-center">
-        <Title /> {/* Usando o componente Title */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0">
-          {cameraUrls.map((camera, index) => (
-            <div key={index} className="relative">
-              <img
-                src={camera.url}
-                alt={camera.lugar}
-                className="w-full h-auto object-cover rounded-none cursor-pointer"
-                onClick={() => handleImageClick(camera.url)}
-              />
-              <div className="absolute bottom-0 left-0 right-0 bg-opacity-10 bg-black text-white p-2 text-sm text-center">
-                {camera.lugar}
-              </div>
-            </div>
-          ))}
-        </div>
+    return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
+  }, []);
 
-        {isFullScreen && <FullScreenImage imageUrl={currentImage} close={closeFullScreen} />}
-      </div>
+  const handleImageClick = (url) => {
+    setCurrentImage(url);
+    setIsFullScreen(true);
+  };
+
+  const closeFullScreen = () => {
+    setIsFullScreen(false);
+    setCurrentImage(null);
+  };
+
+  // Função para gerenciar erros de carregamento de imagem e aplicar fallback
+  const handleImageError = (index) => {
+    setCameraUrls(prevUrls =>
+      prevUrls.map((camera, i) =>
+        i === index ? { ...camera, url: "/path/to/fallback-image.jpg" } : camera
+      )
     );
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-200 p-0 flex flex-col items-center justify-center">
+      <Title /> {/* Usando o componente Title */}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0">
+        {cameraUrls.map((camera, index) => (
+          <div key={index} className="relative">
+            <img
+              src={camera.url}
+              alt={camera.lugar}
+              className="w-full h-auto object-cover rounded-none cursor-pointer"
+              onClick={() => handleImageClick(camera.url)}
+              onError={() => handleImageError(index)} // Aplica fallback em caso de erro
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-opacity-10 bg-black text-white p-2 text-sm text-center">
+              {camera.lugar}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {isFullScreen && <FullScreenImage imageUrl={currentImage} close={closeFullScreen} />}
+    </div>
+  );
 }
 
 export default App;
