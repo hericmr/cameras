5ba8bfa9e5faf2ae377c25d9ab74f3de35e0ef0c
unknown <heric.moura@unifesp.br>
2024-11-13 09:03:45 -0300

    novas cameras adicionadas - encontradas com machine learning

--- src/CameraCard.js
+++ src/CameraCard.js
@@ -1,32 +1,27 @@
-// CameraCard.js
 import React, { useState } from 'react';
 
 function CameraCard({ camera, onImageClick }) {
-    const [previousImages, setPreviousImages] = useState({});
-    
+    const [imageUrl, setImageUrl] = useState(camera.url);
+    const fallbackImageUrl = "URL_DE_FALLBACK";  // Substitua com o caminho para uma imagem padrão
+
     const handleImageError = () => {
-        setPreviousImages(prev => ({
-            ...prev,
-            url: previousImages.url || camera.url,
-        }));
+        setImageUrl(fallbackImageUrl);  // Use uma imagem padrão se o carregamento falhar
     };
 
     const handleImageLoad = () => {
-        setPreviousImages(prev => ({
-            ...prev,
-            url: camera.url,
-        }));
+        // Imagem carregada com sucesso, você pode adicionar alguma lógica, se necessário
     };
 
     return (
         <div className="relative">
             <img
-                src={camera.url}
-                alt={camera.lugar}
+                src={imageUrl}
+                
                 className="w-full h-auto object-cover rounded-none cursor-pointer"
                 onClick={() => onImageClick(camera.url)}
                 onError={handleImageError}
                 onLoad={handleImageLoad}
+                loading="lazy"  // Habilita o lazy loading
             />
             <div className="absolute bottom-0 left-0 right-0 bg-opacity-10 bg-black text-white p-2 text-sm text-center">
                 {camera.lugar}
