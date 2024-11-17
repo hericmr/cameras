aea9b35b2e7b558bfc38f8fbe20fdb89282c679b
unknown <heric.moura@unifesp.br>
2024-11-17 13:26:27 -0300

    visao noturna adicionada

--- src/FullScreenImage.js
+++ src/FullScreenImage.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function FullScreenImage({ imageUrl, close }) {
     const [currentImageUrl, setCurrentImageUrl] = useState(imageUrl);
+    const [isNightVision, setIsNightVision] = useState(false);
 
     useEffect(() => {
         let animationFrameId;
@@ -9,7 +10,7 @@ function FullScreenImage({ imageUrl, close }) {
 
         const updateImage = () => {
             const now = Date.now();
-            if (now - lastUpdate >= 2000) { // Atualiza a cada 100ms
+            if (now - lastUpdate >= 100) {
                 setCurrentImageUrl(`${imageUrl}&t=${new Date().getTime()}`);
                 lastUpdate = now;
             }
@@ -21,19 +22,35 @@ function FullScreenImage({ imageUrl, close }) {
         return () => cancelAnimationFrame(animationFrameId);
     }, [imageUrl]);
 
+    const toggleNightVision = () => {
+        setIsNightVision((prev) => !prev);
+    };
+
     return (
         <div className="fixed inset-0 bg-black bg-opacity-90 flex justify-center items-center z-90">
             <div className="relative w-full h-full">
                 <img
                     src={currentImageUrl}
                     alt="Imagem em tela cheia"
-                    className="w-full h-full object-contain"
+                    className={`w-full h-full object-contain transition-all ${
+                        isNightVision ? "filter hue-rotate-90 brightness-75" : ""
+                    }`}
                 />
+            </div>
+            <div className="absolute top-4 right-4 z-50">
                 <button
                     onClick={close}
-                    className="absolute top-4 right-4 bg-red-600 text-white p-2 rounded-full"
+                    className="bg-red-700 text-white text-sm p-2 rounded-full shadow-lg hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500"
                 >
-                    X
+                    <span className="font-semibold">X</span>
+                </button>
+            </div>
+            <div className="absolute top-4 left-4 z-50">
+                <button
+                    onClick={toggleNightVision}
+                    className="bg-yellow-500 text-black text-sm p-1 rounded-full shadow-lg hover:bg-yellow-600 focus:outline-none focus:ring-1 focus:ring-yellow-500"
+                >
+                    {isNightVision ? "Desativar" : "Visão Noturna"}
                 </button>
             </div>
         </div>
